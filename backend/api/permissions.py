d9854229239a13e2ed0be87a676cdbd1b325488d
serfervrn <serfervrn@yandex.ru>
2022-09-06 09:40:35 +0300

    fix review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -13,7 +13,6 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
