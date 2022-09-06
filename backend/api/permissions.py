d1146f7197a452fc4ed29fe6e6c663b1bee0fb06
serfervrn <serfervrn@yandex.ru>
2022-09-06 09:38:05 +0300

    fix review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,17 +3,17 @@ from rest_framework import permissions
 
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+        return (request.method in permissions.SAFE_METHODS or
+                request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-                or request.user.is_staff)
+        return (request.method in permissions.SAFE_METHODS or
+                obj.author == request.user or
+                request.user.is_staff)
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
